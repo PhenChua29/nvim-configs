77516427951d796435e3d75ed3d33b133e4435e2
Phen Chua <89496400+PhenChua29@users.noreply.github.com>
2024-12-16 09:21:35 +0000

    Change telescope's layout appearance

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -6,7 +6,29 @@ if not ok then
   return
 end
 
-telescope.setup()
+telescope.setup({
+  defaults = {
+    -- Custom texts
+    prompt_prefix = "🔍", 
+    results_title = "🍀",
+
+    path_display = { "filename_first" }, -- Show files' names first, then paths later
+    dynamic_preview_title = true, -- Show file name dynamically on preview window's title
+
+    layout_strategy = "horizontal",
+    sorting_strategy = "ascending", -- Reverse the results
+
+    layout_config = {
+      horizontal = {
+        prompt_position = "top",
+        preview_width = 0.5
+      },
+      center = {
+        preview_cutoff = 1
+      }
+    }
+  }
+})
 
 -- =========== Popup window appearance =========== 
 local api = vim.api
