cbb89ae0f6dbb7d033f3d0a646aa07df643517ab
Phen Chua <89496400+PhenChua29@users.noreply.github.com>
2025-12-09 09:42:12 +0700

    Update obsidian configs

--- lua/user/obsidian.lua
+++ lua/user/obsidian.lua
@@ -6,38 +6,45 @@ if not ok then
 	return
 end
 
-local workspaces = {
-	{
-		name = "University",
-		path = "~/fpt/notes",
-	},
-}
-
-local attachments = {
-	img_folder = "Files",
-}
-
-local daily_notes = {
-	folder = "Journey/Daily",
-}
-
-local templates = {
-	folder = "Template",
-}
-
 obsidian.setup({
-	workspaces = workspaces,
-	attachments = attachments,
-	daily_notes = daily_notes,
-	new_notes_location = "Inbox",
+	notes_subdir = "Notes",
+  new_notes_location = "notes_subdir",
 
-	-- Prefix imgs with timestamp for uniqueness
-	image_name_func = function()
-		-- Prefix image names with timestamp.
-		return string.format("%s-", os.time())
-	end,
+	workspaces = {
+		{
+			name = "University",
+			path = "~/fpt/notes",
+		},
+	},
 
-	templates = templates,
+	attachments = {
+		img_folder = "Files",
+	},
+	daily_notes = {
+		folder = "Journey/Daily",
+	},
+	templates = {
+		folder = "Template",
+	},
 
 	ui = { enable = false },
+
+	---@param title string|?
+	---@return string
+	note_id_func = function(title)
+		-- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+		-- In this case a note with the title 'My new note' will be given an ID that looks
+		-- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+		local suffix = ""
+		if title ~= nil then
+			-- If title is given, transform it into valid file name.
+			suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+		else
+			-- If title is nil, just add 4 random uppercase letters to the suffix.
+			for _ = 1, 4 do
+				suffix = suffix .. string.char(math.random(65, 90))
+			end
+		end
+		return tostring(os.time()) .. "-" .. suffix
+	end,
 })
